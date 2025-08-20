c893ada6c1454102afab605c439e60835a0b3b0c
cheilrainne <angelica.s@cheil.com>
2025-08-20 08:30:01 +0400

    Implement Firebase Project config and trigger image upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2073147527"
+    serviceWorkerVersion: "1969971255"
   }
 });
