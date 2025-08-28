2ee132a049407cd6374eb7c141bd463cf4f2cbea
cheilrainne <angelica.s@cheil.com>
2025-08-28 14:40:17 +0400

    Updated live server endpoints for API Calls

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4213042052"
+    serviceWorkerVersion: "2938335134"
   }
 });
