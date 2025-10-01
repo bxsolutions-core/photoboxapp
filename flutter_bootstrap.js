3d64427bfef4cb83fbc4af6cf9b4d811bed95aca
cheilrainne <angelica.s@cheil.com>
2025-10-01 11:56:15 +0400

    [DEBUG] Adjust offscreen compositor horizontal margin

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "971600115"
+    serviceWorkerVersion: "3786498947"
   }
 });
